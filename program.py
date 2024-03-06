cb1a87dcfbf2a0a3241ba5c85bf5db9d5fd63cbf
Nainjot singh <146698404+Nainjot123456@users.noreply.github.com>
2024-03-06 06:06:53 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,15 +1,1 @@
-class Car:
-    def__init___l"l" r, m):
-        o:
 
-        lf.reg = r
-        self.ms = m
-        self.s = 0
-        self.d = 0
-
-nc = Car("XYZ-456", 180)
-
-print("Registration Number:", nc.reg)
-print("Maximum Speed:", nc.ms, "km/h")
-print("Current Speed:", nc.s, "km/h")
-print("Travelled Distance:",nc.d,"km")-
